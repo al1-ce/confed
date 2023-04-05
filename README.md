e584386d5fcc45e70855eddf45b4ab567ba97e96
al1-ce <al1-ce@list.ru>
2023-04-05 14:20:14 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # confed
 Simple tool to quickly edit configs by typing two words
+
+[![aur](https://img.shields.io/aur/version/confed.svg?logo=archlinux&style=flat-square&logoColor=white)](https://aur.archlinux.org/packages/confed) 
+[![dub](https://img.shields.io/dub/v/confed.svg?logo=d&style=flat-square)](https://code.dlang.org/packages/confed) 
+[![git](https://img.shields.io/github/v/release/al1-ce/confed?label=git&logo=github&style=flat-square)](https://github.com/al1-ce/confed)
+![license](https://img.shields.io/aur/license/confed.svg?style=flat-square)
+![aur votes](https://img.shields.io/aur/votes/confed.svg?style=flat-square) 
+![dub rating](https://badgen.net/dub/rating/confed?style=flat)
+![dub rating](https://badgen.net/github/stars/al1-ce/confed?style=flat)
+
+## Config
+
---
+++ src/app.d
@@ -0,0 +1,171 @@
+import std.stdio;
+import std.getopt;
+import std.array: split, join;
+import std.algorithm: canFind, countUntil;
+import std.conv: to;
+import std.file: exists, mkdir;
+import std.process: spawnShell, wait;
+
+import core.stdc.stdlib: getenv, exit;
+
+import sily.getopt;
+import sily.path: fixPath;
+
+int main(string[] args) {
+    string removeName = "";
+    string setName = "";
+    string editor = "";
+    bool doList = false;
+
+    GetoptResult help = getopt(
+        args,
+        std.getopt.config.bundling, std.getopt.config.passThrough,
+        "set|s", "Sets new path for config", &setName,
+        "add|a", "Alias for set", &setName,
+        "remove|r", "Removes config path", &removeName,
+        "editor|e", "Open with custom editor command", &editor,
+        "list|l", "Lists all configs", &doList
+    );
+
+    if (help.helpWanted) {
+        Option[] usage = [
+            customOption("Open", "confed nvim"),
+            customOption("Set", "confed -s nvim ~/.config/nvim/"),
+            customOption("Remove", "confed -r nvim"),
+            customOption("List", "confed -l"),
+            customOption("Editor", "confed -e \"nvim --noplugin --\" nvim")
+        ];
+        printGetopt("confed [args]", "Options", help.options, "Usage", usage);
+        return 0;
+    }
+    
+    checkPath();
+    Config[] conf = configRead();
+    
+    if (doList) {
+        foreach (Config c; conf) {
+            writeln(c.name, ": \"", c.path, "\"");
+        }
+        return 0;
+    }
+
+    string arg = args[1..$].join();
+
+    if (setName != "") {
+        int namePos = conf.findName(setName);
+        if (namePos != -1) {
+            conf[namePos].path = arg;
+        } else {
+            conf ~= Config(setName, arg);
+        }
+        configWrite(conf);
+        return 0;
+    }
+
+    if (removeName != "") {
+        int namePos = conf.findName(removeName);
+        if (namePos != -1) {
+            Config c = conf[namePos];
+            conf = conf.split(c).join();
+        } else {
+            writeln("Error, can't find config with name \"", removeName, "\".");
+            return 1;
+        }
+        configWrite(conf);
+        return 0;
+    }
+    
+    if (arg == "") {
+        writeln("Error, missing config name.");
+        return 1;
+    }
+
+    int namePos = conf.findName(arg);
+    if (namePos == -1) {
+        writeln("Error, can't find config with name \"", arg, "\".");
+        return 1;
+    }
+
+    editor = editor == "" ? getenv("EDITOR").to!string : editor;
+    
+    // writeln(editor ~ " " ~ conf[namePos].path);
+    wait(spawnShell(editor ~ " " ~ conf[namePos].path));
+
+    return 0;
+}
+
+string configPath = "~/.config/confed/config_list";
+string configPathOnly = "~/.config/confed";
+/* Config structure:
+   Name//////Path
+*/
+
+void checkPath() {
+    if (!configPathOnly.fixPath.exists()) {
+        mkdir(configPathOnly.fixPath);
+    }
+    if (!configPath.fixPath.exists()) {
+        File f = File(configPath.fixPath, "w+");
+        f.close();
+    }
+}
+
+void configWrite(Config[] arr) {
+    string _out;
+    for (int i = 0; i < arr.length; ++i) {
+        Config c = arr[i];
+        _out ~= c.name ~ " // " ~ c.path;
+        if (i + 1 != arr.length) _out ~= "\n";
+    }
+    File f = File(configPath.fixPath, "w+");
+    f.write(_out);
+    f.close();
+}
+
+Config[] configRead() {
+    File f = File(configPath.fixPath, "r+");
+    Config[] arr;
+    int i = 1;
+    while (!f.eof) {
+        string line = f.readln();
+        if (line == "") break;
+        if (!f.eof) line = line[0..$-1]; 
+        string[] l = line.split(" // ");
+
+        if (l.length != 2) {
+            writeln(configPath.fixPath, ":", i, " Error, invalid line, expected \"name // path\", got \"", line, "\".");
+            exit(1);
+        }
+        string name = l[0];
+        string path = l[1];
+        arr ~= Config(name, path);
+        ++i;
+    }
+    f.close();
+    return arr;
+}
+
+int findPath(Config[] cf, string p) {
+    for (int i = 0; i < cf.length; ++i) {
+        Config c = cf[i];
+        if (c.path == p) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int findName(Config[] cf, string n) {
+    for (int i = 0; i < cf.length; ++i) {
+        Config c = cf[i];
+        if (c.name == n) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+struct Config {
+    string name;
+    string path;
+}
